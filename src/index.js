0f3eb4df663ab83f4daa894691a143d0e14917d4
mishaa77 <mishbushuu@gmail.com>
2025-05-22 15:51:24 +0300

    Added axios

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-function generateQuote(event) {
-  event.preventDefault();
+function displayQoute(response) {
   new Typewriter("#quote", {
-    strings: "“You have to believe in yourself when no one else does.”",
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
     delay: 1,
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+
+  let userElement = document.getElementById("enter-input");
+  let apiKey = "900fe54a2dcb23943o88ct0a1d192e13";
+  let prompt = `Generate a qoute about${userElement}`;
+  let context = `you are an AI assistant who creates quotes .Generate quotes based on the ${userElement}.the quote should be relatable and should mention the${userInput}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayQoute);
+  console.log(userElement);
 }
 
 let submitForm = document.getElementById("form");
