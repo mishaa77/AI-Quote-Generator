c10f60a5ca78c3fd5270b82902b65e7c79401ccb
mishaa77 <mishbushuu@gmail.com>
2025-05-22 15:06:39 +0300

    Added basic js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateQuote(event) {
+  event.preventDefault();
+  new Typewriter("#quote", {
+    strings: "“You have to believe in yourself when no one else does.”",
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
 
+let submitForm = document.getElementById("form");
+submitForm.addEventListener("submit", generateQuote);
